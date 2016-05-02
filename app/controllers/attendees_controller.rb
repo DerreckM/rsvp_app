e0c9b51e0063d0fb67713805a71646aa9a2020c7
derreck <mansheim23@hotmail.com>
2016-05-02 15:24:17 -0500

    fixed the attendees controller to add attendee

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,13 +1,13 @@
-class AttendeeController < ApplicationController
+class AttendeesController < ApplicationController
   attr_accessor :date, :body
   def index
     @attendee = Attendee.new
-    @attendees = current_user.items
+    #@attendees = Attendees.new
   end
 
   def create
-    @attendee = current_user.items.create(date: params[:item][:date], body: params[:item][:body])
-    redirect_to attendees_path
+    @attendee = Attendee.create(name: params[:attendee][:name])#, body: params[:item][:body])
+    redirect_to root_path
   end
 
   def update
